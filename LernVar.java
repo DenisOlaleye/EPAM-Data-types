24366a0bf3fdf7844b2e3df116a7002b58fc118c
DenisOlaleye <denis.olaleye@gmail.com>
2022-08-23 06:55:05 +0300

    var переписування

---
+++ LernVar.java
@@ -0,0 +1,18 @@
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class LernVar 
+{
+	public static void main(String[] args) 
+	{
+		var n = 1; //int
+		var i = Integer.valueOf(2);
+		var str = "Java"; //string
+		List<Object> res = List.of(1, "2", 3.14, new StringBuilder("Java10"));
+		List<Object> lst = res;
+		
+		
+	}
+
+}
